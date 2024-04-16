dcd24ec66c9c3cedc320175630a86bd35bde6cd2
mickaeltabo <mickael26t@gmail.com>
2024-04-16 04:19:17 +0200

    main.tfv2

--- main.tf
+++ main.tf
@@ -6,4 +6,9 @@ resource "aws_instance" "example" {
   ami           = "ami-00c71bd4d220aa22a"
   instance_type = "t2.micro"
   key_name      = "DevOps"
+  
+  vpc_settings {
+    vpc_id    = "vpc-00e50b8aeffaba06e"
+    subnet_id = "subnet-070d42d20554771c7"
+  }
 }
